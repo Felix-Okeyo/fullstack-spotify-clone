3d6c306d062d71a1f3e75480ead0a1c9b0c2d060
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-20 16:20:26 +0300

    children passed to main section

--- spotify-clone/components/SidebarItem.tsx
+++ spotify-clone/components/SidebarItem.tsx
@@ -1,8 +1,38 @@
+import { IconType } from "react-icons"
+import  Link  from "next/link";
+import { twMerge } from "tailwind-merge";
 
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href?: string;
+}
 
-const SidebarItem = () =>{
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon: Icon, //write it like this to use it as a component 
+    label, 
+    active, 
+    href}) =>{
+    
   return (
-    <div>SidebarItem</div>
+    
+        <Link 
+        href={href} 
+        className={twMerge(`flex 
+        flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer 
+        hover:text-white transition 
+        text-neutral-400 py-1`, active && "text-white") // style the active route to be white
+        // pass the icon below and this converts the text to icons automatically
+        }>
+
+        <Icon size={26}/> 
+        <p className="truncate w-full">{label}</p>
+        
+        </Link>
+        
+ 
+   
   )
 }
 
