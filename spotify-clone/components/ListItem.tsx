b02b9918f42aa5387cbe15387401378c5be8f2f3
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-21 12:48:31 +0300

    layout done, list of songs done, newest songs too

--- spotify-clone/components/ListItem.tsx
+++ spotify-clone/components/ListItem.tsx
@@ -24,7 +24,8 @@ const ListItem: React.FC<ListItemProps>= (
         router.push(href);
     }
     return (
-        <button className="relative group flex items-center 
+        <button onClick={onClick}
+        className="relative group flex items-center 
         rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 
         hover:bg-neutral-100/20 transition pr-4"
         >
