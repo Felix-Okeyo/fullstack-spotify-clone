c757b520d3f4212c706fd17b8be13e63f674799b
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-21 12:23:50 +0300

    hover, liked button, relative, right-5 added

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           </h1>
         
           <div className=" grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4" >
-            <ListItem image="/images/liked.png" name="Liked songs"/>
+            <ListItem image="/images/liked.png" name="Liked songs" href="liked"/>
           </div>
       
       </div>
