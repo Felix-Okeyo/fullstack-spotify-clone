da1372a9e2dcab1814ba38a69d3ab7f1e7f90fb8
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-23 15:23:46 +0300

    install supabase auth helpers for react and nextjs

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+// import the db
+import { database } from "@/types_db";
+
+interface SupabaseProviderProps {
+    children: React.ReactNode;
+};
+
+const SupabaseProvider: React.FC<SuperbaseProviderProps> = ({
+    children
+}) => {
+    
+}
