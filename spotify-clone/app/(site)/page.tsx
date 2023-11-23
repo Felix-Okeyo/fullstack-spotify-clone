9ed1c8322a9296e3c382200a9480ab6b1399353c
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-23 13:21:11 +0300

    supabase password,txt gitignored

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
 
-export default function Home() {
+const Home=()=> {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto"> 
     <Header>
@@ -30,3 +30,4 @@ export default function Home() {
     </div>
   )
 }
+export default Home
