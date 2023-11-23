c0cb718d34ecfb12f95c7474e099324902b3b8d9
Felix-Okeyo <felixokeyo20@gmail.com>
2023-11-23 15:53:01 +0300

    hooks folder, client wrapped in supabseprovider,

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
 import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
 
 const font = Figtree({ subsets: ['latin'] }) //rename import from font to figtree
 
@@ -20,10 +21,12 @@ export default function RootLayout({
       <body className={font.className}>
         {/* wrap the layout of the app with the sidebar 
         component kind of like you do with the navbar */}
-      
-        <Sidebar> 
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar> 
+            {children}
+          </Sidebar>
+        </SupabaseProvider>
+        
       </body>
     </html>
   )
