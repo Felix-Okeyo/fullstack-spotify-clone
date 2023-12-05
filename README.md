d075b90983d6e9bb090250cdef3b721136609c69
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-05 12:24:27 +0300

    mps song input added and styled

--- README.md
+++
@@ -1,2 +0,0 @@
-# fullstack-spotify-clone
-This repository contains files covering a Spotify clone application  
--- spotify-clone/components/UploadModal.tsx
+++ spotify-clone/components/UploadModal.tsx
@@ -1,16 +1,36 @@
 "use client";
 
+import {FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Modal from './Modal'
 import useUploadModal from '@/hooks/useUploadModal';
+import { useState } from 'react';
+import Input from './Input';
 
 function UploadModal() {
 
     const uploadModal = useUploadModal();
+    const {register,handleSubmit,reset} = useForm<FieldValues>({
+        defaultValues: {
+            author:'',
+            title:'',
+            song: null,
+            image: null,
+        }
+    }); 
+    const [isLoading, setIsLoading ] = useState();
+
     const onChange = (open: boolean) => {
         if(!open){
             // Reset/roll back the form
+            reset();
+
             uploadModal.onClose();
         }
+
+    }
+
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        //upload song to supabase server
 
     }
 
@@ -19,7 +39,15 @@ function UploadModal() {
         description='Upoload an mp3 file'
         isOpen ={uploadModal.isOpen}
         onChange={onChange}>
-        Form
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Input 
+            id ='title'
+            disabled ={isLoading}
+            {...register('title', { required: true })}
+            placeholder ="Song title"
+            />
+
+        </form>
     </Modal>
   )
 }
