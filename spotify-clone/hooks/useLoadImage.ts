288ce3d623ebbf3821dcf4e87637aa1181295a95
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-05 15:15:12 +0300

    songs loaded and done

--- spotify-clone/hooks/useLoadImage.ts
+++ spotify-clone/hooks/useLoadImage.ts
@@ -4,15 +4,13 @@ import { Song } from "@/types";
 
 const useLoadImage = (song: Song) => {
   const supabaseClient = useSupabaseClient();
-  
+  //check if there is a song
   if (!song) {
     return null;
   }
 
-  const { data: imageData } = supabaseClient
-    .storage
-    .from('images')
-    .getPublicUrl(song.image_path);
+    //get the image data or url and remap it 
+  const { data: imageData } = supabaseClient.storage.from('images').getPublicUrl(song.image_path);
 
   return imageData.publicUrl;
 };
