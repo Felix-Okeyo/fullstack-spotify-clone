c8472bfd8ae9996cdaa777a2598be2f041fc9d81
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-05 16:06:43 +0300

    play button debugged

--- spotify-clone/components/PlayButton.tsx
+++ spotify-clone/components/PlayButton.tsx
@@ -2,12 +2,13 @@ import { FaPlay } from "react-icons/fa";
 
 const PlayButton = () => {
   return ( 
-    <button title = 'play music button' className="transition opacity-0 rounded-full flex items-center 
-        justify-centerbg-green-500 
+    <button  
+    className="transition opacity-0 rounded-full flex items-center 
+        justify-center bg-green-500 
         p-4 drop-shadow-md translate
         translate-y-1/4 group-hover:opacity-100 
         group-hover:translate-y-0 hover:scale-110 " >
-      <FaPlay className="text-black" />
+      <FaPlay className="text-black" title = 'play music button'/>
     </button>
    );
 }
