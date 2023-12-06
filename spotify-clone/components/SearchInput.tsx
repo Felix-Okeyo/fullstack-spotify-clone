c25e235bc72a767261609da96ecefd67869aa137
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-06 19:39:54 +0300

    tested the search input, url changes after 5 sec

--- spotify-clone/components/SearchInput.tsx
+++ spotify-clone/components/SearchInput.tsx
@@ -1,12 +1,35 @@
 "use client";
+import qs from "query-string"
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Input from "./Input";
 
 const SearchInput = () => {
     //give the component functionalities
+    const router = useRouter();
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500); 
+
+    //install querystring
+    //create useeffect
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        }
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query
+        });
+        router.push(url);
+    },[debouncedValue, router]);
 
     return (
-        <div>
-            Search Input 
-        </div>
+        <Input 
+        placeholder="What do you wanna listen to"
+        value={value}
+        onChange={(e)=>{setValue(e.target.value)}}/>
     )
 }
 
