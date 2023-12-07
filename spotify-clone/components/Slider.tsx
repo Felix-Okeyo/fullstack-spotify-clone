149c6379cb0589967ebdb4af715f664a7af49a81
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-07 15:09:19 +0300

    slider added and styled

--- spotify-clone/components/Slider.tsx
+++ spotify-clone/components/Slider.tsx
@@ -2,10 +2,39 @@
 
 import * as RadixSlider from "@radix-ui/react-slider";
 
+interface SliderProps {
+    value?: number;
+    onChange?: (value: number) => void;
+}
 
-const Slider = () => {
-  return (
-    <div>Slider</div>
+const Slider:React.FC<SliderProps> = ({
+    value=1, 
+    onChange
+}) => {
+
+    const handleChange = (newValue: number[])=>{
+        // take the first value
+        onChange?.(newValue[0])
+    }
+    
+    return (
+    <RadixSlider.Root
+        className="relative flex items-center select-none touch-none w-full h-10"
+        defaultValue={[1]}
+        value={[value]}
+        onValueChange={handleChange}
+        max={1}
+        step={0.1}
+        arial-label="Volume">
+            <RadixSlider.Track
+                className="bg-neutral-600 relative grow rounded-full h-[3px]">
+                    <RadixSlider.Range
+                        className="absolute bg-white rounded-full h-full">
+
+                    </RadixSlider.Range>
+            </RadixSlider.Track>
+
+    </RadixSlider.Root>
   )
 }
 
