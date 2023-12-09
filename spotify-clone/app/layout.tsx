b1619422b1db811877e39184d368b4b2eff50100
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-09 12:41:14 +0300

    error.tsx and loading.tsx for logging in error, react-spinner installed

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -24,6 +24,9 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const userSongs = await getSongsByUserId();
+    
+  // throw new Error('Test')
+
   return (
     <>
       <html lang="en">
