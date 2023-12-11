5e23e1e5ccca9ecf815e075c71b92b9ba6058e8d
Felix-Okeyo <felixokeyo20@gmail.com>
2023-12-11 22:28:06 +0300

    session object debugs

--- spotify-clone/app/api/create-checkout-session/route.ts
+++ spotify-clone/app/api/create-checkout-session/route.ts
@@ -38,7 +38,7 @@ export async function POST(
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       billing_address_collection: 'required',
-      customer:'stripe_customer_id',
+      customer,
       line_items: [
         {
           price: price.id,
@@ -48,7 +48,7 @@ export async function POST(
       mode: 'subscription',
       allow_promotion_codes: true,
       subscription_data: {
-        trial_from_plan: true,
+        trial_period_days: 7,
         metadata
       },
       success_url: `${getURL()}/account`,
